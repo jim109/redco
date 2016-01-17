8edd1f65e4a1c6a0b06cfebb73883b0db8e74140
jim109 <josma109@gmail.com>
2016-01-16 23:33:58 -0500

    Agregando base de datos

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect("mongodb://localhost/redco_db")
+
+//Definir el esquema de nuestro productos o la tabla
+var productSchema = {
+	title:String,
+	description:String,
+	imageUrl:String,
+	pricing:Number
+};
+
+//Modelo
+var Product = mongoose.model("Product", productSchema);
+
+
 app.set("view engine", "jade");
 
 app.use(express.static("public"));
 
 app.get("/", function(solicitud,respuesta){
+
+	var data = {
+		title: "Mi primer super producto",
+		description: "Una mega",
+		imageUrl:"data.png",
+		pricing: 10
+	}
+
+	var product = new Product(data);
+
+	product.save(function(err){
+		console.log(product);
+	});
+
 	respuesta.render("index")
 });
 //HTTP
