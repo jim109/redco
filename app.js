eb71459a8a9ebcb07bdcf24733105817a6c9255f
jim109 <josma109@gmail.com>
2016-01-17 23:39:50 -0500

    Agregando formularios

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 mongoose.connect("mongodb://localhost/redcodb");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Definir el esquema de nuestro productos o la tabla
 var productSchema = {
@@ -23,7 +27,7 @@ app.use(express.static("public"));
 
 app.get("/",function(solicitud,respuesta){
 
-	var data = {
+	/*var data = {
 		title: "Mi primer super producto",
 		description: "Una mega",
 		imageUrl: "data.png",
@@ -34,13 +38,40 @@ app.get("/",function(solicitud,respuesta){
 
 	product.save(function(err){
 		console.log(product);
-	});
+	});*/
 
 	respuesta.render("index");
 });
-//HTTP
-	//Metodos
-	//GET
-	//POST
+
+app.post("/productos", function(solicitud, respuesta){
+	
+	if(solicitud.body.password == "123"){
+		var data = {
+			title: solicitud.body.title,
+			description: solicitud.body.description,
+			imageUrl: "data.png",
+			pricing: solicitud.body.pricing
+		}
+
+		var product = new Product(data);
+
+		product.save(function(err){
+			console.log(product);
+			respuesta.render("index");
+		});
+
+	}else{
+		respuesta.render("productos/new");
+	}
+
+
+
+
+});
+
+app.get("/productos/new", function(solicitud, respuesta){
+
+	respuesta.render("productos/new")
+});
 
 app.listen(8080);
